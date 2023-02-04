f1d22e3294d4a6c985974574de8c54acd9242078
john <muffinpost1@gmail.com>
2023-02-04 16:47:53 -0600

    add sample routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Banner from './Components/Banner';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
-import Test from './Components/Test';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test />}/>
+        <Route path="/" element={<div>homepage - myapps</div>}/>
+        <Route path="/app/:appId" element={<div>app page</div>}/>
+        <Route path="/app/:appId/permissions" element={<div>app page - permissions tab</div>}/>
+        <Route path="/app/:appId/users" element={<div>app page - users tab</div>}/>
       </Routes>
     </BrowserRouter>
   );
