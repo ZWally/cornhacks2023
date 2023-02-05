02e9cab5d09c4e87e35c9025a68e44db3bf5eba2
Roy Dumblauskas <roydumblauskas@gmail.com>
2023-02-04 23:43:36 -0600

    Added Empty Brackets to MyAppsPage.tsx to stop infinite loop

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,13 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
 import AppUser from './Types/AppUser';
+import MyAppPage from './Components/MyAppsPage/MyAppsPage';
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>homepage - myapps</div>}/>
-        <Route path="/app/:appId" element={<div>app page</div>}/>
-        <Route path="/app/:appId/permissions" element={<div>app page - permissions tab</div>}/>
-        <Route path="/app/:appId/users" element={<div>app page - users tab</div>}/>
-      </Routes>
-    </BrowserRouter>
+    <MyAppPage/>
   );
 }
 
