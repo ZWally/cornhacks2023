c43e5923b0812aaf8b010f9a10a363311ef2ab8a
john <muffinpost1@gmail.com>
2023-02-05 04:44:48 -0600

    finish adding roles/perms and showing them

--- src/Components/MatrixTable.tsx
+++ src/Components/MatrixTable.tsx
@@ -2,6 +2,7 @@ import Permission from "../Types/Permission"
 import {TableContainer, Table, TableHead, TableRow, TableBody, TableCell, Checkbox} from "@mui/material"
 import Role from "../Types/Role"
 import { updateRole } from "../utils/database"
+import React from "react"
 
 type Props = {
     permissions: Permission[]
@@ -10,13 +11,21 @@ type Props = {
 }
 
 const MatrixTable = ({permissions, roles, setRoles}: Props) => {
-    console.log(roles)
+    const [unsavedRoles, setUnsavedRoles] = React.useState([...roles]);
+    
+    React.useEffect(() => {
+        console.log("EFFECT IN PROGRESS");
+        if (unsavedRoles.length < roles.length) {
+            setUnsavedRoles([...unsavedRoles, roles[roles.length - 1]]);
+        }
+    }, [roles])
+    
     return (
         <TableContainer>
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell>test</TableCell>
+                        <TableCell></TableCell>
                         {roles.map(role => <TableCell>{role.name}</TableCell>)}
                     </TableRow>
                 </TableHead>
@@ -26,11 +35,24 @@ const MatrixTable = ({permissions, roles, setRoles}: Props) => {
                         return (
                         <TableRow>
                             <TableCell>{permission.name}</TableCell>
-                            {roles.map(role => {
+                            {unsavedRoles.map((role, roleIndex) => {
                                 const hasPerm = role.permissionIds.includes(permission.id)
                                 return (
                                     <TableCell>
-                                        <Checkbox checked={hasPerm}/>
+                                        <Checkbox checked={hasPerm}
+                                            onClick={() => {
+                                                if (hasPerm) {
+                                                    const permIndex = unsavedRoles[roleIndex].permissionIds.findIndex((permId => permId === permission.id));
+                                                    let newRoles = [...unsavedRoles]
+                                                    newRoles[roleIndex].permissionIds.splice(permIndex, 1);
+                                                    setUnsavedRoles(newRoles);
+                                                } else {
+                                                    let newRoles = [...unsavedRoles]
+                                                    newRoles[roleIndex].permissionIds.push(permission.id)
+                                                    setUnsavedRoles(newRoles);
+                                                }
+                                            }}
+                                        />
                                     </TableCell>
                                 )
                             })}
