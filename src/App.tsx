c26acd2e9b91a0d5ea84c13bcf1a5c60dbbf224c
john <muffinpost1@gmail.com>
2023-02-04 18:30:32 -0600

    update material ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,36 @@ import Banner from './Components/Banner';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
+import UsersTable from './Components/UsersTable';
+import AppUser from './Types/AppUser';
+
+const dummyUsers: AppUser [] = [
+  {
+    id: "1",
+    authId: "John Poost",
+    roleId: "1"
+  },
+  {
+    id: "2",
+    authId: "Rooy Dummy",
+    roleId: "2"
+  },
+  {
+    id: "3",
+    authId: "Zach Wallemnerbg",
+    roleId: "3"
+  },
+  {
+    id: "4",
+    authId: "Lanmdry Geibger",
+    roleId: "4"
+  },
+]
 
 function App() {
   return (
+    <>
+    <UsersTable users={dummyUsers}/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>homepage - myapps</div>}/>
@@ -15,6 +42,7 @@ function App() {
         <Route path="/app/:appId/users" element={<div>app page - users tab</div>}/>
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
