dfc15073ba8b599f2c172b964dd6b05a6b22c124
Landry Geiger <landry.geiger@gmail.com>
2023-02-04 18:07:50 -0600

    More database function stubs

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,16 +4,9 @@ import Permission from '../Types/Permission';
 import Role from '../Types/Role';
 import SiteUser from '../Types/SiteUser';
 import { db } from "./firebase";
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, DocumentData, getDoc, QueryDocumentSnapshot } from 'firebase/firestore';
 
-export async function getAppUserbyId(appUserId: string): Promise<AppUser | null> {
-  return new Promise<AppUser | null>(async (resolve, reject) => {
-    const docRef = await getDoc(doc(db, 'appUsers', appUserId));
-
-    if (!docRef.exists()) {
-      return reject(null);
-    }
-    
+function getAppUserFromDocRef(docRef: QueryDocumentSnapshot<DocumentData>) {
     const docData = docRef.data()
 
     const appUser: AppUser = {
@@ -22,57 +15,99 @@ export async function getAppUserbyId(appUserId: string): Promise<AppUser | null>
       authId: docData['authId'],
     }
 
-    return resolve(appUser);
+    return appUser;
+}
+
+export async function getAppUserbyId(appUserId: string): Promise<AppUser> {
+  return new Promise<AppUser>(async (resolve, reject) => {
+    const docRef = await getDoc(doc(db, 'appUsers', appUserId));
+
+    if (!docRef.exists()) {
+      return reject();
+    }
+    
+    return resolve(getAppUserFromDocRef(docRef));
   });
 }
 
-export async function createAppUser(roleId: string, authId: string): Promise<AppUser | null> {
-  return new Promise<AppUser | null>(async (resolve, reject) => {
-
+export async function getAppUsersById(appUserIds: string []): Promise<AppUser []> {
+  return new Promise<AppUser []>(async (resolve, reject) => {
+    Promise.all(appUserIds.map(getAppUserbyId)).then(appUsers => {
+      resolve(appUsers)
+    }).catch(reject);
   });
 }
 
-export async function getAppById(appId: string): Promise<App | null> {
-  return new Promise<App | null>(async (resolve, reject) => {
-
-  });
-}
-
-export async function createApp(userIds: string [], roleIds: string [], permissionIds: string []): Promise<App | null> {
-  return new Promise<App | null>(async (resolve, reject) => {
-
-  });
-}
-
-export async function getPermissionById(permissionId: string): Promise<Permission | null> {
-  return new Promise<Permission | null>(async (resolve, reject) => {
-
-  });
-}
-
-export async function createPermission(name: string): Promise<Permission | null> {
-  return new Promise<Permission | null>(async (resolve, reject) => {
+export async function createAppUser(roleId: string, authId: string): Promise<AppUser> {
+  return new Promise<AppUser>(async (resolve, reject) => {
 
   });
 }
 
-export async function getRoleById(roleId: string): Promise<Role | null> {
-  return new Promise<Role | null>(async (resolve, reject) => {
+export async function getAppById(appId: string): Promise<App> {
+  return new Promise<App>(async (resolve, reject) => {
 
   });
 }
 
-export async function createRole(name: string, permissionIds: string []): Promise<Role | null> {
-  return new Promise<Role | null>(async (resolve, reject) => {
+export async function getAppsById(appIds: string []): Promise<App []> {
+  return new Promise<App []>(async (resolve, reject) => {
 
   });
 }
 
-export async function getSiteUserById(siteId: string): Promise<SiteUser | null> {
-  return new Promise<SiteUser | null>(async (resolve, reject) => {
+export async function createApp(userIds: string [], roleIds: string [], permissionIds: string []): Promise<App> {
+  return new Promise<App>(async (resolve, reject) => {
 
   });
 }
 
+export async function getPermissionById(permissionId: string): Promise<Permission> {
+  return new Promise<Permission>(async (resolve, reject) => {
+
+  });
+}
+
+export async function getPermissionsById(permissionIds: string []): Promise<Permission []> {
+  return new Promise<Permission []>(async (resolve, reject) => {
+
+  });
+}
+
+export async function createPermission(name: string): Promise<Permission> {
+  return new Promise<Permission>(async (resolve, reject) => {
+
+  });
+}
+
+export async function getRoleById(roleId: string): Promise<Role> {
+  return new Promise<Role>(async (resolve, reject) => {
+
+  });
+}
+
+export async function getRolesById(roleIds: string []): Promise<Role []> {
+  return new Promise<Role []>(async (resolve, reject) => {
+
+  });
+}
+
+export async function createRole(name: string, permissionIds: string []): Promise<Role> {
+  return new Promise<Role>(async (resolve, reject) => {
+
+  });
+}
+
+export async function getSiteUserById(siteUserId: string): Promise<SiteUser> {
+  return new Promise<SiteUser>(async (resolve, reject) => {
+
+  });
+}
+
+export async function getSiteUsersById(siteUserIds: string): Promise<SiteUser []> {
+  return new Promise<SiteUser []>(async (resolve, reject) => {
+
+  });
+}
 
 
