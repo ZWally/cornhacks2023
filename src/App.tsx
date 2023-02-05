c7362408ceea0b1e3724bb749790b5c77df0d470
john <muffinpost1@gmail.com>
2023-02-04 21:35:58 -0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,41 +8,9 @@ import AppUser from './Types/AppUser';
 import Role from './Types/Role';
 import UsersTable from './Components/UsersTable';
 
-const dummyUsers: AppUser [] = [
-  {
-    id: "1",
-    authId: "John Poost",
-    roleId: "1"
-  },
-  {
-    id: "2",
-    authId: "Rooy Dummy",
-    roleId: "1"
-  },
-  {
-    id: "3",
-    authId: "Zach Wallemnerbg",
-    roleId: "1"
-  },
-  {
-    id: "4",
-    authId: "Lanmdry Geibger",
-    roleId: "1"
-  },
-]
-
-const dummyRoles: Role [] = [
-  {
-    id: "1",
-    name: "Admin",
-    permissionIds: []
-  }
-]
-
 function App() {
   return (
     <>
-    <UsersTable roles={dummyRoles} users={dummyUsers}/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>homepage - myapps</div>}/>
